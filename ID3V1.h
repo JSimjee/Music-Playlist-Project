4c7d42889e8fea13b55ab4ba4c36eadfcac3741b
JSimjee <151388604+JSimjee@users.noreply.github.com>
2024-06-13 16:44:03 -0700

    First upload yay

---
+++ ID3V1.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+struct ID3v1 {
+  char _tag[3];
+  char _title[30];
+  char _artist[30];
+  char _album[30];
+  char _year[4];
+  char _comment[30];
+  char _genre[1];
+
+  bool valid() const { return tag() == "TAG"; }
+  std::string tag() const { return std::string(_tag,3); }
+  std::string title() const { return std::string(_title,30); }
+  /*
+  1. Shouldn't the bool and two strings contribute to the size of an ID3V1 object?
+  2. How do you construct all of these data members? You don't have a manually defined constructor, so where is it coming from and how does it work alongside the "int f" and "int n" in main?
+  */
+};
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "ID3V1.h"
+
+using namespace std;
+
+int main() {
+  // This is not a char pointer nor a char array like in struct ID3V1, it's a string pointer
+  const char *mp3 = "/home/js/Documents/Projects/Music Project for Donte/Sample Music Files/80. Space Junk Galaxy.mp3";
+  
+  // Is this O_RDONLY literally just a 0 value? 
+  // So this fancy syntax about ::open and the parameters link to some strange function in /usr/include/x86_64-linux-gnu, this is just something you know from experience in industry?
+  int f = ::open(mp3,O_RDONLY);
+  cout << f << ' ' << sizeof(ID3v1) << endl;
+
+  ID3v1 header;
+  int n = ::read(f,&header,128);
+  cout << n << endl;
+  cout << header.tag() << endl;
+  
+}
+  
