17e650faa67fd7a2f68043e2132e816b318c4316
JSimjee <jameelsimjee@gmail.com>
2024-06-17 18:42:00 -0700

    update

--- ID3V1.h
+++ ID3V1.h
@@ -3,17 +3,18 @@
 #include <string>
 
 struct ID3v1 {
-  char _tag[3];
-  char _title[30];
-  char _artist[30];
-  char _album[30];
-  char _year[4];
-  char _comment[30];
-  char _genre[1];
+  char properties[128];
+  // char _tag[3];
+  // char _title[30];
+  // char _artist[30];
+  // char _album[30];
+  // char _year[4];
+  // char _comment[30];
+  // char _genre[1];
 
-  bool valid() const { return tag() == "TAG"; }
-  std::string tag() const { return std::string(_tag,3); }
-  std::string title() const { return std::string(_title,30); }
+  // bool valid() const { return tag() == "TAG"; }
+  // std::string tag() const { return std::string(_tag,3); }
+  // std::string title() const { return std::string(_title,30); }
   /*
   1. Shouldn't the bool and two strings contribute to the size of an ID3V1 object?
   2. How do you construct all of these data members? You don't have a manually defined constructor, so where is it coming from and how does it work alongside the "int f" and "int n" in main?
--- main.cpp
+++ main.cpp
@@ -13,10 +13,9 @@ using namespace std;
 
 int main() {
   // This is a constant char pointer that points to an array of chars. "mp3" can also be read as a string
-  const char *mp3 = "/Sample Music Files/80. Space Junk Galaxy.mp3";
+  const char *mp3 = "Sample Music Files/80. Space Junk Galaxy.mp3";
   
-  // Is this O_RDONLY literally just a 0 value? 
-  // So this fancy syntax about ::open and the parameters link to some strange function in /usr/include/x86_64-linux-gnu, this is just something you know from experience in industry?
+  // f will be negative or 2 if the OS can't open the file. Otherwise, the integer f is used to refer to a table of files this process has opened, starting from 3.
   int f = ::open(mp3,O_RDONLY);
   cout << f << ' ' << sizeof(ID3v1) << endl;
 
@@ -26,7 +25,11 @@ int main() {
 
 
   cout << n << endl;
-  cout << header.tag() << endl;
+  // cout << header.tag() << endl;
+  // cout << header.title() << endl;
+
+  // Why does outputting a 128 char array work the same as outputting a 3 char array? "properties" should have all 128 bytes from the file, and should output all of them.
+  cout << header.properties << endl;
   
 }
   
